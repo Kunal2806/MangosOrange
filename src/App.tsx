dc5c11c04ae06de98e843ea49a708606fce7b611
root <root@DESKTOP-M70RUTN.localdomain>
2025-06-04 06:53:46 +0000

    navbar update

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import Nav from "./component/Nav"
 const App = () => {
   return (
     <div className="min-h-screen w-screen">
-      <div className="fixed">
+      <div className="fixed top-0 left-0 backdrop-blur-xl">
         <Nav/>
       </div>
-      <div id="home">
+      <div id="home" className="mt-[80px]">
         <Home/>
       </div>
     </div>
